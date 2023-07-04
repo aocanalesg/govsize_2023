b4f9ef4eaaf50957c85cea5a813b5745926bc1de
aocanalesg <135900750+aocanalesg@users.noreply.github.com>
2023-07-04 11:30:42 -0600

    R manana

--- dofiles/GOV_SIZE.R
+++ dofiles/GOV_SIZE.R
@@ -1,17 +1,108 @@
 #Investigacion: Impacto del tamano del Gobierno en el crecimiento Economico 
 #Fecha: Julio 4, 2023
 
+#CLEAR
+rm(list = ls())
+
+#Workind directory
 setwd('/Users/axelcanales/Documents/GitHub/govsize_2023')
+#Packages
+#install.packages("googlesheets4")
+#install.packages("timeSeries")
+#install.packages("zoo")
+#install.packages("xts")
+library(xts)
+library(dplyr)
+library(googlesheets4)
+library(lubridate)
+library(zoo)
 
 #Importar data (Euler)
 
+raw_data <- read_sheet("https://docs.google.com/spreadsheets/d/15_lA3MjsOMDQinHgw2A93T7tTmHdqEpOQGHSFFtkpIU/edit?usp=sharing",
+           sheet = "RAW_DATA2",
+           col_names = TRUE,
+           range = "A1:H65"
+           )
 
 
-#Calculo de datos (Euler)
+#Limpieza datos (Euler)
 
+#rename
+raw_data <- raw_data %>%
+  rename( 
+    "date" = "DATE",
+    "gdp" = "RAW_GDP",
+    "gov_con" = "RAW_GOV_CON",
+    "pub_inv" = "RAW_PUB_INV",
+    "priv_inv" = "RAW_PRIV_INV",
+    "x" = "RAW_X",
+    "m" = "RAW_M",
+    "pop" = "RAW_POP"
+  )
+
+var_names_bcn <- c("date", "gdp", "gov_con", "pub_inv", "priv_inv", "x", "m", "pop")
+
+
+
+
+#rescale variables from BCN to millions of cords
+raw_data <- raw_data %>%
+  mutate(
+    gdp = gdp*10^6,
+    gov_con =gov_con*10^6,
+    pub_inv= pub_inv*10^6,
+    priv_inv=priv_inv*10^6,
+    x=x*10^6,
+    m=m*10^6
+         )
 
 
 #Backast (Tony Stark)
+
+#Variables as share of PIB per capita
+raw_data <- raw_data %>%
+  mutate(
+    gdp_pc = gdp/pop,
+    gov_gdp = (gov_con + pub_inv)/gdp,
+    gov_con_gdp = gov_con/gdp,
+    pub_inv_gdp= pub_inv/gdp,
+    priv_inv_gdp = priv_inv/gdp,
+    tr_op = (x+m)/gdp,
+  )
+
+
+
+#Time series set
+ts_vars <- ts(data = raw_data,
+              start = c(2006,1),
+              frequency = 4
+)
+
+ts_vars[,1] <- as.yearqtr(ts_vars[,1],           # Convert dates to quarterly
+           format = "%Y-%m-%d")
+
+#create dummies 
+tt <- seq(as.Date("1959-01-01"), as.Date("1990-12-01"), by='mon')
+tsMloa_x <- xts(unclass(tsMloa)[,1:3], order.by=tt)
+
+tsMloa_x$meanLog <- tsMloa_x$log - mean(tsMloa_x$log)
+
+ts_vars[,"dummy_2008"] <- seq(0) 
+
+
+dat$dummy[which(dat$date >= "2018-01-01")] <- 1
+
+
+replace d_2008 = 1 if date >= tq(2008q3) & date <= tq(2009q1)
+label variable d_2008 "Dummy variable that captures international financial crisis"
+
+gen d_2018 = 0
+replace d_2018 = 1 if date > tq(2018q1) 
+label variable d_2018 ""
+
+ts_vars$dummie1 = ifelse(date > ,1,0)
+
 
 
 
