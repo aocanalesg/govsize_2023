190442a0e602517010879e4c8c0d612f7cba5154
aocanalesg <135900750+aocanalesg@users.noreply.github.com>
2023-07-04 08:14:59 -0600

    R master script

---
+++ dofiles/GOV_SIZE.R
@@ -0,0 +1,74 @@
+#Investigacion: Impacto del tamano del Gobierno en el crecimiento Economico 
+#Fecha: Julio 4, 2023
+
+setwd('/Users/axelcanales/Documents/GitHub/govsize_2023')
+
+#Importar data (Euler)
+
+
+
+#Calculo de datos (Euler)
+
+
+
+#Backast (Tony Stark)
+
+
+
+#Desestacionalizacion (Done)
+
+
+
+#Estacionariedad (Tony Stark)
+
+
+
+
+#Incluye montar la tabla y exportarla a codigo latex
+#Criterio de seleccion de rezagos Gasto Agregado (Tony Stark)
+
+
+
+#Criterio de seleccion de rezagos Inversion Fija Publica (Tony Stark)
+
+
+
+#Prueba de presedencia temporal Gasto Agregado  (Tony Stark)
+
+
+
+
+#Prueba de presedencia temporal Inversion Fija Publica (Tony Stark)
+
+
+
+
+#Prueba de cointegracion (Euler)
+
+
+#Ecuacion de largo plazo Gasto Agregado (Tony Stark)
+
+
+
+
+#Ecuacion de largo plazo Inversion Fija (Euler)
+
+
+
+
+#Estimacion del tamano optiomo (Euler)
+
+
+
+
+#Bootstrap (Together but Tony Stark leading)
+
+
+
+
+
+
+
+
+
+
